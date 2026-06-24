cb14cb7a829c948515aa46ee374cbda2ececdaae
/ Nin <starpastly@gmail.com>
2026-06-24 14:23:55 +0400

    card draw simulator

--- Game3.py
+++ Game3.py
@@ -6,4 +6,5 @@ numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 selected_number = random.choice(numbers)
 selected_suit = random.choice(suits)
 
+
 print("Your card is:", selected_number, "of", selected_suit)
