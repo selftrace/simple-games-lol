eda50a9190bddd8cf397a329bcd1ce06979bfe8a
/ Nin <starpastly@gmail.com>
2026-06-24 14:20:52 +0400

    Add files via upload

---
+++ Game3.py
@@ -0,0 +1,9 @@
+import random
+
+suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
+numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+selected_number = random.choice(numbers)
+selected_suit = random.choice(suits)
+
+print("Your card is:", selected_number, "of", selected_suit)
