4eb2b449b7150f820ddbc7c74dcc237c2ffc5808
/ Nin <starpastly@gmail.com>
2026-06-24 14:23:26 +0400

    dice roll game

--- Game2.py
+++ Game2.py
@@ -6,6 +6,7 @@ player2_roll = random.randint(1, 6)
 print("Player 1 rolled:", player1_roll)
 print("Player 2 rolled:", player2_roll)
 
+
 if player1_roll > player2_roll:
     print("Player 1 wins!")
 elif player2_roll > player1_roll:
